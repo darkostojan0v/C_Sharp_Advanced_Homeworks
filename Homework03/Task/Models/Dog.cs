ff3ea2e35339ddef89dd1d772af8604ec71e00a3
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-04-25 20:38:48 +0200

    Homework03

---
+++ Homework03/Task/Models/Dog.cs
@@ -0,0 +1,23 @@
+﻿
+using Task.Enums;
+
+namespace Task
+{
+	public class Dog
+	{
+		public string Name { get; set; }
+		public string Color { get; set; }
+		public int Age { get; set; }
+		public Race Race { get; set; }
+
+
+		public Dog(string name, string color, int age, Race race)
+		{
+			Name = name;
+			Color = color;
+			Age = age;
+			Race = race;
+		}
+
+	}
+}
